1689cdd6bfdfbef094922ca83f5aa4be6838b811
saimaAnis21 <saima_rehman21@yahoo.com>
2021-05-17 20:27:46 +0500

    Set game template

---
+++ src/Scenes/BootScene.js
@@ -0,0 +1,16 @@
+import 'phaser';
+import shipImg from '../assets/ship.png';
+ 
+export default class BootScene extends Phaser.Scene {
+  constructor () {
+    super('Boot');
+  }
+ 
+  preload () {
+    this.load.image('logo', shipImg);
+  }
+ 
+  create () {
+    this.scene.start('Preloader');
+  }
+};
